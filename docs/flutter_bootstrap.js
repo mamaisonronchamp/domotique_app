f85f5d19670d7f314f0a5b0cfafe5576b795c516
mamaisonronchamp <ba.lacroix@laposte.net>
2026-05-03 10:32:08 +0200

    update app

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "682886669" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3114807470" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
